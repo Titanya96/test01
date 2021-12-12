cd9ebf6bc3e2d7658b4f308ec1e0e3be754ba9d1
Анна <annatitova5555@yandex.ru>
2021-12-13 01:05:11 +0300

    test message

---
+++ src/Calculator.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // сканер общий для ввода
+
+        /** TASK 1
+         * вывести первую и последнюю буквы строки.
+         */
+//        char chAny = string.charAt(string.length() - 1);
+//        System.out.println(chAny);
+        /*
+        string "abcdef"
+        index:  012345
+        length: кол-во элементов
+         */
+
+        String string = scanner.nextLine();
+        System.out.println(string.charAt(0));
+        System.out.println(string.charAt(string.length() - 1));
+
+/**
+ *  вывести результаты в виде String, а не int.
+ *  не забывать про типы данных, которые отдают методы.
+ */
+
+        System.out.println("Первое целое число: ");
+        int a = scanner.nextInt();
+        System.out.println("Вы ввели: " + a);
+        System.out.println("Второе целое число: ");
+        int b = scanner.nextInt();
+        System.out.println("Вы ввели: " + b);
+        System.out.println("Что с ними сделать? ");
+        String act = scanner.next();
+        System.out.println("Результат = " + result(a, b, act));
+    }
+
+    static String result(int a, int b, String act) {
+        switch (act) {
+            case "+":
+                return String.valueOf(a + b);
+            case "-":
+                return String.valueOf(a - b);
+            case "*":
+                if (a == 0 || b == 0) {
+                    return String.valueOf(0);
+                } else {
+                    return String.valueOf(a * b);
+                }
+            case "/":
+                if (b == 0) {
+                    return byZero(a);
+                } else {
+                    return String.valueOf(a / b);
+                }
+            default:
+                return String.valueOf(0);
+        }
+    }
+
+    static String byZero(int a) {
+        return String.valueOf(a * a);
+    }
+}
